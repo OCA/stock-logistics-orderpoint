6f7626c5c4f764b7c55185c6d9e016b36be069f9
Mmequignon <matthieu@fwzte.xyz>
2025-07-03 18:35:50 +0200

    [FIX] stock_location_orderpoint: fix migration script

--- stock_location_orderpoint/migrations/16.0.1.1.1/post-migrate.py
+++ stock_location_orderpoint/migrations/16.0.1.1.1/post-migrate.py
@@ -6,5 +6,5 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
-        env.cr, "stock_location_orderpoint", "data/queue_job_function.xml", mode="init"
+        env, "stock_location_orderpoint", "data/queue_job_function.xml", mode="init"
     )
