edb53d827ee580254cd7d0e4af6fadfd47912968
Jacques-Etienne Baudoux <je@bcim.be>
2026-02-12 17:55:12 +0100

    [FIX] sale_stock_orderpoint_mto_as_mts: run auto
    Run newly created auto orderpoint

--- sale_stock_orderpoint_mto_as_mts/models/sale_order_line.py
+++ sale_stock_orderpoint_mto_as_mts/models/sale_order_line.py
@@ -19,17 +19,31 @@ class SaleOrderLine(models.Model):
 
     def _ensure_default_orderpoints_for_mto(self):
         mto_products_by_wh = defaultdict(lambda: self.product_id.browse())
+        origins_by_wh_by_prod = defaultdict(lambda: defaultdict(list))
         for sol in self:
             for move in self.move_ids:
                 route = sol.route_id or move.rule_id.route_id
                 if (
                     route.is_mto
                     and move.picking_type_id.code == "outgoing"
-                    and move.state not in ("done", "cancel")
+                    and move.state not in ("draft", "done", "cancel")
                 ):
                     wh = move.warehouse_id or sol.order_id.warehouse_id
                     mto_products_by_wh[wh] |= move.product_id
+                    origins_by_wh_by_prod[wh][move.product_id].append(move.origin)
         for warehouse, mto_products in mto_products_by_wh.items():
-            mto_products._ensure_default_orderpoint_for_mto(
+            new_orderpoints = mto_products._ensure_default_orderpoint_for_mto(
                 domain=[], warehouse=warehouse
             )
+            # As orderpoint are created after the SO demand has been placed, we
+            # need to run them if they are in auto trigger
+            ops = new_orderpoints.filtered(lambda op: op.trigger == "auto")
+            if ops and not self.env["ir.config_parameter"].sudo().get_param(
+                "stock.no_auto_scheduler"
+            ):
+                origins = {
+                    op.id: origins_by_wh_by_prod[warehouse][op.product_id] for op in ops
+                }
+                ops.with_context(origins=origins)._procure_orderpoint_confirm(
+                    company_id=warehouse.company_id, raise_user_error=False
+                )
