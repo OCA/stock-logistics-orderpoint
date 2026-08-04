c6ed96ee5075e65218ad43bfdf6e8be0c06001ed
Clément Mombereau <clement.mombereau@akretion.com>
2026-08-04 16:01:35 +0200

    [MIG] stock_orderpoint_generator: Basic Migration to version 18.0

--- stock_orderpoint_generator/__manifest__.py
+++ stock_orderpoint_generator/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Order point generator",
     "summary": "Mass configuration of stock order points",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp, Tecnativa, Odoo Community Association (OCA)",
     "category": "Warehouse",
     "license": "AGPL-3",
